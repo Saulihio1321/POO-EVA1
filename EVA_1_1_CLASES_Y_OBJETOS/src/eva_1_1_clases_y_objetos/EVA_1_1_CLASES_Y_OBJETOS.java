a818df28bb26bbcf5611733f36c8acc1493f9d1a
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-02-02 22:33:26 -0600

    Add files via upload

---
+++ EVA_1_1_CLASES_Y_OBJETOS/src/eva_1_1_clases_y_objetos/EVA_1_1_CLASES_Y_OBJETOS.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_1_1_clases_y_objetos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_1_CLASES_Y_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona perso1;
+        //solo eh declarado la variable, no eh creado el objeto
+        //crear clase (intanciar) consiste en asignar memoria.
+        int valor;
+    
+    }
+    
+}
+
+//clase persona
+//NombreDeLaClase --- Nomenglatura Camelcase
+class Persona{
+    
+   
+}
